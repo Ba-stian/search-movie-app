9ae0c29ecdb34aa5cf58765054137e8ff1eb141f
Ba-stian <k.batakovich@gmail.com>
2020-08-12 23:58:02 +0300

    Fix config for environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,46 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const dotenv = require('dotenv');
 
-module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.join(__dirname, '/dist'),
-		filename: 'bundle.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: 'babel-loader',
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
+module.exports = () => {
+	const env = dotenv.config().parsed;
+
+	const envKeys = Object.keys(env).reduce((prev, next) => {
+		prev[`process.env.${next}`] = JSON.stringify(env[next]);
+		return prev;
+	}, {});
+	return {
+		entry: './src/index.js',
+		output: {
+			path: path.join(__dirname, '/dist'),
+			filename: 'bundle.js',
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use: 'babel-loader',
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader'],
+				},
+			],
+		},
+		devServer: {
+			host: '127.0.0.1',
+			port: 4000,
+		},
+		plugins: [
+			new CleanWebpackPlugin(),
+			new HtmlWebpackPlugin({
+				template: './public/index.html',
+				favicon: './public/glasses.png',
+			}),
+			new webpack.HotModuleReplacementPlugin(),
+			new webpack.DefinePlugin(envKeys),
 		],
-	},
-	devServer: {
-		host: '127.0.0.1',
-		port: 4000,
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			template: './public/index.html',
-			favicon: './public/glasses.png',
-		}),
-		new webpack.HotModuleReplacementPlugin(),
-	],
+	};
 };

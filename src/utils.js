e6db4ffec578833ac3e4a31bc4afe45994863c9b
Ba-stian <k.batakovich@gmail.com>
2020-08-13 12:51:29 +0300

    Fix for url

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const fetchMovie = async (searchValue) => {
-	return await axios.get(`htp://www.omdbapi.com/?t=${searchValue}&apikey=${process.env.API_KEY}&`);
+	return await axios.get(`http://www.omdbapi.com/?t=${searchValue}&apikey=${process.env.API_KEY}&`);
 };
 
 export {

58005a107def3cb0f8365088455c4da251e522bb
Ba-stian <k.batakovich@gmail.com>
2020-08-13 10:36:39 +0300

    Refactor util

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,7 @@
 const axios = require('axios');
 
 const fetchMovie = async (searchValue) => {
-	await axios.get(`http://www.omdbapi.com/?t=${searchValue}&apikey=${process.env.API_KEY}&`)
-		.then((res) => {
-			if (res.status === 200 && res.data.length) {
-				return res.data;
-			}
-			return res;
-		});
+	return await axios.get(`htp://www.omdbapi.com/?t=${searchValue}&apikey=${process.env.API_KEY}&`);
 };
 
 export {

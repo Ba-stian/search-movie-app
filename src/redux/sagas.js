faf8ef4a33d9df12bcd54616d8eec3a370851c5b
Ba-stian <k.batakovich@gmail.com>
2020-08-13 10:37:14 +0300

    Add ability, to fetch data in saga

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,11 +1,22 @@
-import { takeEvery, put, call } from 'redux-saga/effects';
+import {
+	takeEvery, put, call, select,
+} from 'redux-saga/effects';
 import { requestMovies, moviesRequested } from './actions';
 import fetchMovie from '../utils';
 
+const getSearchValue = (state) => state.movies.searchValue;
+
 function* sagaWorker() {
 	yield put(requestMovies());
-	const data = yield call(fetchMovie);
-	yield put(moviesRequested(data));
+	const searchValue = yield select(getSearchValue);
+	try {
+		const res = yield call(fetchMovie, searchValue);
+		if (res.status === 200) {
+			yield put(moviesRequested(res.data));
+		}
+	} catch (e) {
+		yield put({ type: 'REQUEST_FAILED', payload: e.toString() });
+	}
 }
 
 function* sagas() {
